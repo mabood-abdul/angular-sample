fa46640e57089fba715727660e3bcd1963988a2c
mabood-abdul <abdul.mabood@outlook.com>
2022-03-30 03:36:03 +0530

    A new product added - Phone SE

--- src/app/products.ts
+++ src/app/products.ts
@@ -23,6 +23,12 @@ export const products = [
     name: 'Phone Standard',
     price: 299,
     description: ''
+  },
+  {
+    id: 4,
+    name: 'Phone SE',
+    price: 299,
+    description: ''
   }
 ];
 
